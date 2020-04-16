251bd59d53b17ed342b1ee229d5ad76238b3fc5b
Neale Van Stralen <nealevanstrn@gmail.com>
2020-04-15 21:55:09 -0400

    Created docker image files for development.

---
+++ docker/ros2-dev/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+CMD_ARGS=("$@")
+
+source /opt/ros/dashing/setup.sh
+
+LOCAL_SETUP=${PWD}/install/setup.bash
+if [ -f "$LOCAL_SETUP" ]; then
+    source $LOCAL_SETUP
+fi
+
+exec "${CMD_ARGS[@]}"
